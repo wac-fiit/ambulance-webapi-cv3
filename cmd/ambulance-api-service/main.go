164d79e6ce5877875eb3a0347c149f7496de0fde
Michal Sevcik <michal.sevcik@protonmail.com>
2024-04-02 11:26:05 +0200

    Implementation of web api

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -5,6 +5,12 @@ import (
 	"os"
 	"strings"
 
+	"context"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/wac-fiit/ambulance-webapi-cv3/internal/db_service"
+
 	"github.com/gin-gonic/gin"
 	"github.com/wac-fiit/ambulance-webapi-cv3/api"
 	"github.com/wac-fiit/ambulance-webapi-cv3/internal/ambulance_wl"
@@ -22,6 +28,24 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
 
 	// request routings
 	ambulance_wl.AddRoutes(engine)
